6464e650af58f085acb90770c1890de20285544f
unknown <allbegj@gmail.com>
2025-04-03 14:00:22 +0500

    add PostList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, {useState} from 'react';
 import Counter from "./components/Counter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
+    const [posts, usePosts] = useState([
+        {id: 1, title: 'JS 1', body: 'Desc 1'},
+        {id: 2, title: 'JS 2', body: 'Desc 2'},
+        {id: 3, title: 'JS 3', body: 'Desc 3'},
+    ])
 
   return (
       <div className="App">
-          <PostItem post={{id: 1, title: 'JS', body: 'Desc'}}/>
+          <PostList
+              posts={posts}
+              title={'Список постов'}/>
       </div>
   );
 }
