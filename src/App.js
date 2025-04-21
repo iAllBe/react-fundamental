bff41dcd08e06fc683c313ecd1c4b3df7e324bb3
unknown <allbegj@gmail.com>
2025-04-21 18:58:44 +0500

    create setchPosts in useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,17 +6,18 @@ import PostFilter from "./components/UI/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'AAA', body: 'CCC'},
-        {id: 2, title: 'CCC', body: 'BBB'},
-        {id: 3, title: 'BBB', body: 'AAA'},
-    ])
+    const [posts, setPosts] = useState([])
 
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+    useEffect(() => {
+        fetchPosts()
+    }, []);
 
     function createPost(newPost) {
         setPosts([...posts, newPost])
@@ -25,6 +26,11 @@ function App() {
 
     function removePost(post) {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
     }
 
   return (
