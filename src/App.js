ec601f7c5fe2bd97cd75ff085742b04f7f69c20b
unknown <allbegj@gmail.com>
2025-04-29 10:33:02 +0500

    create useFetching

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [isPostingLoading, setIsPostingLoading] = useState(false)
+    const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll()
+        setPosts(posts)
+    })
 
     useEffect(() => {
         fetchPosts()
@@ -27,15 +31,6 @@ function App() {
 
     function removePost(post) {
         setPosts(posts.filter(p => p.id !== post.id))
-    }
-
-    async function fetchPosts() {
-        setIsPostingLoading(true)
-        setTimeout(async () => {
-            const posts = await PostService.getAll()
-            setPosts(posts)
-            setIsPostingLoading(false)
-        }, 1000)
     }
 
   return (
@@ -57,7 +52,8 @@ function App() {
               filter={filter}
               setFilter={setFilter}
           />
-          {isPostingLoading
+          {postError && <h1>Произошла ошибка ${postError}</h1>}
+          {isPostLoading
               ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
               : <PostList
                   posts={sortedAndSearchedPosts}
